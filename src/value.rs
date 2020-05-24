64d3743522476f1d8447d181c6764c3787010452
Erkki Keränen <erkki.keranen@gmail.com>
2020-05-24 16:43:44 +0300

    debug comment updates

--- src/value.rs
+++ src/value.rs
@@ -364,11 +364,17 @@ impl Value {
                                 ),
                             );
                             // TODO remove println
-                            println!("defined symbol with docstring meta: {:#?}", ss.name);
+                            println!(
+                                "defined symbol named {} with docstring : {}",
+                                ss.name, &doc_string
+                            );
                             ss.clone()
                         } else {
                             // TODO remove println
-                            println!("cloned symbol, no docstring given: {:#?}", sym.clone().name);
+                            println!(
+                                "cloned symbol name {}, no docstring given.",
+                                sym.clone().name
+                            );
                             sym.clone()
                         };
                         environment.insert(s.to_owned(), defval);
